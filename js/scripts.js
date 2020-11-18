6a5798db5b9e4c37e05f2c2e02ab6a88c48713b6
berkayykorkmaz <berkay@tubu.io>
2020-11-18 23:53:24 +0300

    new version

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,59 @@
 var app = new Vue({
   el: '#app',
   data: {
-    isCurfew: false,
-    details: {},
-    dateTime: new Date().toLocaleString()
+    ageGroups: ['0-20', '21-64', '65+'],
+    ageGroup: 0,
+    working: true,
+    forbidden: false,
+    checked: false,
+    timestamp: ''
   },
-  mounted() {
-    const self = this;
-    const currTime = Math.floor(Date.now() / 1000);
-    curfews.forEach(function (curfew, index) {
-      if (currTime > curfew.start && currTime < curfew.end) {
-        self.details = curfew;
-        self.isCurfew = true;
+  computed: {
+    isWeekend() {
+      let d = new Date();
+      let weekends = ['Sunday', 'Saturday']
+      return weekends.includes(d.getDay()) 
+    },
+    hour() {
+      let d = new Date();
+      return d.getHours()
+    }
+  },
+  created() {
+    setInterval(this.getDate, 1000);
+  },
+  methods: {
+    selectAgeGroup(idx) {
+      this.ageGroup = idx
+    },
+    getDate() {
+      let d = new Date();
+      let locale = d.toLocaleString('tr-TR', {hour12: false})
+      this.timestamp = locale;
+    },
+    retry() {
+      this.checked = false
+      this.ageGroup = 0
+      this.forbidden = false
+      this.working = true
+    },
+    checkForbidden() {
+      this.checked = true
+      if(this.working) {
         return
       }
-    });
+      if(this.isWeekend) {
+        if( this.hour > 20 && this.hour < 10 ) {
+          this.forbidden = true
+        } 
+      } else {
+        if( this.ageGroup === 0 && this.hour > 16 && this.hour < 13 ) {
+          this.forbidden = true
+        } else if(this.ageGroup === 2 && this.hour > 13 && this.hour < 10) {
+          this.forbidden = true
+        }
+      }
+    }
+    
   }
 })
-
-function updateTime() {
-  app.dateTime = new Date().toLocaleString();
-}
-
-setInterval(updateTime, 1000);
